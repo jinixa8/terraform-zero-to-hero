7f23c14abfd632f011560ea2b00a679228be0bd3
jinixa8 <banugaws@gmail.com>
2025-07-25 18:52:32 -0400

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"  # Set your desired AWS region
 }
 
-resource "aws_instance" "t2.micro" {
+resource "aws_instance" "example" {
     ami           = "ami-0d1b5a8c13042c939"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
