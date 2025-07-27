394f3e806d95e0873e1220745c1c31f9495a9cf1
jinixa8 <banugaws@gmail.com>
2025-07-27 16:01:42 -0400

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,4 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-0d1b5a8c13042c939" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-044b41835d0ffa511". # replace this
 }
